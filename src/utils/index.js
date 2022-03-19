c8d40863d2e90686323358df81dc721677bdf37d
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-20 00:48:39 +0700

    make postgres notesservice

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line object-curly-newline
+// eslint-disable-next-line camelcase
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  // eslint-disable-next-line camelcase
+  created_at,
+  // eslint-disable-next-line camelcase
+  updated_at,
+}) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
