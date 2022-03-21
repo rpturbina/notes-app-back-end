1b343a7765c5ade5f5247ba39b0ff310f0642517
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-21 13:11:39 +0700

    protect notes and authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -4,29 +4,44 @@ const routes = (handler) => [
     path: '/notes',
     // * postNoteHandler hanya menerima dan menyimpan "satu" note.
     handler: handler.postNoteHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes',
     // * getNotesHandler mengembalikan "banyak" note.
     handler: handler.getNotesHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes/{id}',
     // * getNoteByIdHandler mengembalikan "satu" note.
     handler: handler.getNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
     // * putNoteByIdHandler hanya menerima dan mengubah "satu" note.
     handler: handler.putNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
     handler: handler.deleteNoteByIdHandler,
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
 ];
 
